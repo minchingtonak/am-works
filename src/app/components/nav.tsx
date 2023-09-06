3c9b83ed738e668a963628362776315b86b49363
Andrew Whitely <andrewwhitely@Andrews-MBP-2.attlocal.net>
2023-09-06 19:23:29 -0400

    update

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -49,7 +49,7 @@ const Nav = () => {
                       {name}
                       {path === pathname ? (
                         <motion.div
-                          className="absolute h-[1px] top-7 mx-2 inset-0 bg-neutral-200 dark:bg-neutral-800 z-[-1] dark:bg-gradient-to-r from-transparent to-neutral-900"
+                          className="absolute h-[1px] top-7 mx-2 inset-0 bg-accent dark:bg-accent z-[-1] dark:bg-gradient-to-r from-transparent to-neutral-900"
                           layoutId="nav"
                           transition={{
                             type: 'spring',
