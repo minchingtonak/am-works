bedb57397295dd9379265d3cc69e888a33d9f07b
Alec Minchington <alec.minch@gmail.com>
2025-05-30 17:56:04 -0700

    add email in footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -16,7 +16,8 @@ function SocialLink({ href, icon: Icon }) {
 
 function SocialLinks() {
   return (
-    <div className="flex text-lg gap-3.5 float-right transition-opacity duration-300 hover:opacity-90">
+    <div className="flex items-center text-lg gap-3.5 float-right transition-opacity duration-300 hover:opacity-90">
+      <p className="text-sm">contact@minch.cc</p>
       <SocialLink href={socialLinks.linkedin} icon={FaLinkedinIn} />
       <SocialLink href={socialLinks.github} icon={FaGithub} />
       <SocialLink href={socialLinks.email} icon={TbMailFilled} />
